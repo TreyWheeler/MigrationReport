ee9869087e7220b8cba7a31d908a047bcf6a3ff3
TreyWheeler <Warenth@gmail.com>
2025-11-01 08:50:08 -0500

    chore: support jest config under esm

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,7 @@
-export default {
+module.exports = {
   testEnvironment: 'jsdom',
   testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
   roots: ['<rootDir>/tests', '<rootDir>/data/tests'],
-  setupFilesAfterEnv: ['<rootDir>/tests/setupJest.js']
+  setupFilesAfterEnv: ['<rootDir>/tests/setupJest.js'],
+  clearMocks: true,
 };
